1814fb0a2bb9c9ecbd7c67b19807df446d404b66
ivil2021 <89508514326@mail.ru>
2024-10-30 22:28:40 +0300

    first commit

---
+++ variables/variables-task1.js
@@ -0,0 +1,14 @@
+/*
+Задача 1: 
+Объявите три переменные с именами name, age и isStudent. 
+Присвойте им ваши имя (строка), возраст (число) и значение true, если вы студент, или false, если нет. 
+Затем выведите значения всех трёх переменных в консоль.
+*/
+
+let name = 'Илья';
+let age = 42;
+let isStudent = false;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
