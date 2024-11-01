ea16ae70757ed62949d98c23685fbdb5150b5d66
ivil2021 <89508514326@mail.ru>
2024-11-02 00:10:13 +0300

    feat(Operators):PMS-1 Work On Tasks On The Topic Operators

---
+++ operators/operators-task4.js
@@ -0,0 +1,19 @@
+/*
+Задача 4: 
+Создайте переменную balance и присвойте ей значение 100. 
+Затем используйте операторы +=, -=, *=, и /= для изменения значения balance. Выведите результат после каждого действия в консоль.
+*/
+
+let balance = 100;
+
+balance += 50;
+console.log(balance);
+
+balance -= 50;
+console.log(balance);
+
+balance *= 50;
+console.log(balance);
+
+balance /= 50;
+console.log(balance);
