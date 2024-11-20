8af3f0a82e168d1992330129efef517172a7aca1
ivil2021 <89508514326@mail.ru>
2024-11-21 00:39:23 +0300

    feat(ForEach):PMS-2 Create Function That Show Each Array Element In Upper Case

---
+++ 07-work-with-arrays/04-forEach/forEach-task2.js
@@ -0,0 +1,9 @@
+/*
+Задача 2: 
+У вас есть массив строк [ "apple", "banana", "cherry" ]. 
+Используя forEach, выведите каждую строку в верхнем регистре.
+*/
+
+const fruits = [ "apple", "banana", "cherry" ];
+
+fruits.forEach(item => console.log(item.toUpperCase()));
