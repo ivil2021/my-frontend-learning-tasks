cf3958138338210f723f2c8841d9a31617830b57
ivil2021 <89508514326@mail.ru>
2024-11-21 00:20:55 +0300

    feat(Filter):PMS-1-1 Create Function Filtered Array Element Greater 10 With Arrow Function

---
+++ 07-work-with-arrays/02-filter/filter-task1-1.js
@@ -0,0 +1,11 @@
+/*
+Задача 1: 
+У вас есть массив чисел [5, 12, 8, 130, 44]. 
+Отфильтруйте только числа больше 10.
+*/
+
+const numbers = [5, 12, 8, 130, 44];
+
+const filteredNumbers = numbers.filter(item => item > 10);
+
+console.log(filteredNumbers);
