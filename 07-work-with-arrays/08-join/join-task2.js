991f322f2ab231a7c735604d15588014e86c8ee8
ivil2021 <89508514326@mail.ru>
2024-11-22 23:24:57 +0300

    feat(Join):PMS-2 Create Function That Concatenate Letters From Array With Symbol  Between Each Of Them

---
+++ 07-work-with-arrays/08-join/join-task2.js
@@ -0,0 +1,11 @@
+/*
+Задача 2: 
+Дан массив ["a", "b", "c", "d"].
+Используя join, создайте строку, где элементы массива разделены символом ``.
+*/
+
+const letters = ["a", "b", "c", "d"];
+
+const concatenatedString = letters.join("``");
+
+console.log(concatenatedString);
