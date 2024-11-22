8dde4d6a82b0551e82444f7062e33b08e437c4d0
ivil2021 <89508514326@mail.ru>
2024-11-22 23:32:42 +0300

    feat(Join):PMS-3 Create Function That Concatenate Strings From Array With Space Between Each Of Them

---
+++ 07-work-with-arrays/08-join/join-task3.js
@@ -0,0 +1,12 @@
+/*
+Задача 3: 
+В массиве
+["I", "love", "JavaScript"]
+объедините все слова в одну строку с использованием join, добавив пробелы между словами.
+*/
+
+const strings = ["I", "love", "JavaScript"];
+
+const concatenatedString = strings.join(" ");
+
+console.log(concatenatedString);
