1f4665538aaa0d4f847f972dbf6195f615a9a73f
ivil2021 <89508514326@mail.ru>
2024-11-22 23:15:26 +0300

    feat(Join):PMS-1 Create Function That Concatenate Strings With Space Between Each Of Them

---
+++ 07-work-with-arrays/08-join/join-task1.js
@@ -0,0 +1,11 @@
+/*
+Задача 1: 
+У вас есть массив строк ["apple", "banana", "cherry"].
+Используя join, создайте строку, где все элементы массива будут объединены через пробел.
+*/
+
+const fruits = ["apple", "banana", "cherry"];
+
+const concatenatedString = fruits.join(" ");
+
+console.log(concatenatedString);
