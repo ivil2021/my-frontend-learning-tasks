6faf730572c248c8f5c293541c580a2da727e2e5
ivil2021 <89508514326@mail.ru>
2024-11-27 22:58:38 +0300

    feat(Split):PMS-1 Create Function That Gets Array Of Words From String And Returns It

---
+++ 07-work-with-arrays/09-split/split-task1.js
@@ -0,0 +1,10 @@
+/*
+Задача 1: 
+Напишите функцию getWords(sentence),
+которая принимает строку sentence и возвращает массив всех слов.
+Считайте, что слова разделены пробелами.
+*/
+
+const getWords = (sentence) => console.log(sentence.split(" "));
+
+getWords("I love JavaScript");
