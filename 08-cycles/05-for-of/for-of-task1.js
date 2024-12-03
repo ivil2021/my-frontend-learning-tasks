35c5b24648400138b0883c9cd509034442d7bb92
ivil2021 <89508514326@mail.ru>
2024-12-03 23:10:13 +0300

    feat(ForOf):PMS-1 Create Function That Show All Elements Of Array

---
+++ 08-cycles/05-for-of/for-of-task1.js
@@ -0,0 +1,10 @@
+/*
+Задача 1: 
+Используя for...of, выведите все элементы массива [1, 2, 3, 4, 5].
+*/
+
+const numbers = [1, 2, 3, 4, 5];
+
+for (let number of numbers) {
+    console.log(number);
+}
