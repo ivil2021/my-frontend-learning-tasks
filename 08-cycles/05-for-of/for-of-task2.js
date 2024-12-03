d98f1cebbcd050a057bca2731312abde1d373443
ivil2021 <89508514326@mail.ru>
2024-12-03 23:20:10 +0300

    feat(ForOf):PMS-2 Create Function That Show Each Letter Of String In New Line

---
+++ 08-cycles/05-for-of/for-of-task2.js
@@ -0,0 +1,11 @@
+/*
+Задача 2: 
+Переберите строку "JavaScript" с помощью for...of
+и выведите каждый символ в новой строке.
+*/
+
+const simpleString = "JavaScript";
+
+for (let letter of simpleString) {
+    console.log(letter);
+}
